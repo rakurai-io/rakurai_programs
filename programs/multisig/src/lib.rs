c2f35bd852d2b1d5247922389db4813a6dcd7a1b
Farakh Ali <126810719+farakhali1@users.noreply.github.com>
2025-03-05 12:41:48 +0500

    feature/multisig_program (#1)
    * updated crate version
    * add vote state program to fix stack size issue if deserialize within same function
    * move rakurai multisig program from native rust to anchor
    * enhance multisig program flexibility and user interaction

---
+++ programs/multisig/src/lib.rs
@@ -0,0 +1,421 @@
+use anchor_lang::prelude::*;
+#[cfg(not(feature = "no-entrypoint"))]
+use {default_env::default_env, solana_security_txt::security_txt};
+
+use crate::{
+    state::{Config, MultiSigAccount},
+    ErrorCode::Unauthorized,
+};
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    // Required fields
+    name: "Rakurai MultiSig Program",
+    project_url: "https://rakurai.io/",
+    contacts: "https://rakurai.io/company",
+    policy: "https://rakurai.io/faq",
+    // Optional Fields
+    preferred_languages: "en",
+    source_code: "https://github.com/rakurai-io/rakurai_programs",
+    source_revision: default_env!("GIT_SHA", "GIT_SHA_MISSING"),
+    source_release: default_env!("GIT_REF_NAME", "GIT_REF_NAME_MISSING")
+}
+pub mod sdk;
+pub mod state;
+
+declare_id!("ArEru7KcVzvVzgukQnJhZE4xsAq43bjz2ZcL1C7Wq9d");
+
+#[program]
+pub mod multi_sig {
+    use rakurai_vote_state::VoteState;
+
+    use super::*;
+    use crate::ErrorCode::*;
+
+    /// Initialize a singleton instance of the [Config] account.
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        authority: Pubkey,
+        block_builder_authority: Pubkey,
+        block_builder_commission_account: Pubkey,
+        block_builder_commission_bps: u16,
+        max_validator_commission_bps: u16,
+        bump: u8,
+    ) -> Result<()> {
+        let cfg = &mut ctx.accounts.config;
+        cfg.authority = authority;
+        cfg.block_builder_authority = block_builder_authority;
+        cfg.block_builder_commission_account = block_builder_commission_account;
+        cfg.block_builder_commission_bps = block_builder_commission_bps;
+        cfg.max_validator_commission_bps = max_validator_commission_bps;
+        cfg.bump = bump;
+        cfg.validate()?;
+
+        Ok(())
+    }
+
+    /// Update config fields. Only the [Config] authority can invoke this.
+    pub fn update_config(ctx: Context<UpdateConfig>, new_config: Config) -> Result<()> {
+        UpdateConfig::auth(&ctx)?;
+
+        let config = &mut ctx.accounts.config;
+        config.authority = new_config.authority;
+        config.block_builder_authority = new_config.block_builder_authority;
+        config.block_builder_commission_account = new_config.block_builder_commission_account;
+        config.max_validator_commission_bps = new_config.max_validator_commission_bps;
+        config.block_builder_commission_bps = new_config.block_builder_commission_bps;
+        config.validate()?;
+
+        emit!(ConfigUpdatedEvent {
+            authority: ctx.accounts.authority.key(),
+        });
+
+        Ok(())
+    }
+
+    /// Initialize a new [MultiSigAccount] associated with the given validator vote key
+    /// and current epoch.
+    pub fn initialize_multi_sig_account(
+        ctx: Context<InitializeMultiSigAccount>,
+        validator_commission_bps: u16,
+        bump: u8,
+    ) -> Result<()> {
+        if validator_commission_bps > ctx.accounts.config.max_validator_commission_bps {
+            return Err(MaxCommissionFeeBpsExceeded.into());
+        }
+
+        if ctx.accounts.validator_vote_account.owner != &solana_program::vote::program::id() {
+            return Err(Unauthorized.into());
+        }
+
+        let validator_vote_state = VoteState::deserialize(&ctx.accounts.validator_vote_account)?;
+        if &validator_vote_state.node_pubkey != ctx.accounts.signer.key {
+            return Err(Unauthorized.into());
+        }
+
+        let multisig_account = &mut ctx.accounts.multisig_account;
+        multisig_account.validator_vote_account = ctx.accounts.validator_vote_account.key();
+        multisig_account.validator_commission_bps = validator_commission_bps;
+        multisig_account.block_builder_commission_bps =
+            ctx.accounts.config.block_builder_commission_bps;
+        multisig_account.block_builder_commission_account =
+            ctx.accounts.config.block_builder_commission_account;
+        multisig_account.validator_authority = ctx.accounts.signer.key();
+        multisig_account.is_enabled = false;
+        multisig_account.proposer = Some(ctx.accounts.signer.key());
+        multisig_account.bump = bump;
+        multisig_account.validate()?;
+
+        emit!(MultiSigAccountInitializedEvent {
+            multisig_account: multisig_account.key(),
+        });
+
+        Ok(())
+    }
+
+    pub fn update_multi_sig_approval(
+        ctx: Context<UpdateMultiSigApproval>,
+        grant_approval: bool,
+    ) -> Result<()> {
+        UpdateMultiSigApproval::auth(&ctx)?;
+
+        let multisig_account = &mut ctx.accounts.multisig_account;
+
+        if grant_approval {
+            if multisig_account.proposer.is_none()
+                || multisig_account.proposer == Some(ctx.accounts.signer.key())
+            {
+                multisig_account.proposer = Some(ctx.accounts.signer.key());
+            } else if multisig_account.validator_authority == ctx.accounts.signer.key()
+                || ctx.accounts.config.block_builder_authority == ctx.accounts.signer.key()
+            {
+                multisig_account.proposer = None;
+                multisig_account.is_enabled = true;
+            } else {
+                return Err(AccountValidationFailure.into());
+            }
+        } else {
+            multisig_account.is_enabled = false;
+        }
+
+        multisig_account.validate()?;
+
+        emit!(UpdateMultiSigApprovalEvent {
+            multisig_account: multisig_account.key(),
+        });
+
+        Ok(())
+    }
+
+    pub fn update_multi_sig_commission(
+        ctx: Context<UpdateMultiSigCommission>,
+        validator_commission_bps: Option<u16>,
+    ) -> Result<()> {
+        UpdateMultiSigCommission::auth(&ctx)?;
+
+        let multisig_account = &mut ctx.accounts.multisig_account;
+
+        if multisig_account.proposer.is_none()
+            || multisig_account.proposer == Some(ctx.accounts.signer.key())
+        {
+            multisig_account.proposer = Some(ctx.accounts.signer.key());
+        } else if multisig_account.validator_authority == ctx.accounts.signer.key()
+            || ctx.accounts.config.block_builder_authority == ctx.accounts.signer.key()
+        {
+            multisig_account.proposer = None;
+            multisig_account.is_enabled = true;
+        } else {
+            return Err(AccountValidationFailure.into());
+        }
+
+        if let Some(bps) = validator_commission_bps {
+            if bps > ctx.accounts.config.max_validator_commission_bps {
+                return Err(MaxCommissionFeeBpsExceeded.into());
+            }
+            multisig_account.validator_commission_bps = bps;
+        }
+
+        multisig_account.block_builder_commission_bps =
+            ctx.accounts.config.block_builder_commission_bps;
+        multisig_account.block_builder_commission_account =
+            ctx.accounts.config.block_builder_commission_account;
+
+        emit!(UpdateMultiSigCommissionEvent {
+            multisig_account: multisig_account.key(),
+            operator_commission: multisig_account.validator_commission_bps,
+            block_builder_commission: multisig_account.block_builder_commission_bps,
+        });
+
+        Ok(())
+    }
+
+    pub fn close_multi_sig_account(ctx: Context<CloseMultiSigAccount>) -> Result<()> {
+        CloseMultiSigAccount::auth(&ctx)?;
+
+        let multisig_account = &mut ctx.accounts.multisig_account;
+
+        let amount = MultiSigAccount::claim_expired(
+            multisig_account.to_account_info(),
+            ctx.accounts.validator_vote_account.to_account_info(),
+        )?;
+        emit!(MultiSigAccountClosedEvent {
+            multisig_account: multisig_account.key(),
+            amount_claimed: amount,
+        });
+
+        Ok(())
+    }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Account failed validation.")]
+    AccountValidationFailure,
+
+    #[msg("Encountered an arithmetic under/overflow error.")]
+    ArithmeticError,
+
+    #[msg("Supplied invalid parameters.")]
+    InvalidParameters,
+
+    #[msg("The given proof is invalid.")]
+    InvalidProof,
+
+    #[msg("Failed to deserialize the supplied vote account data.")]
+    InvalidVoteAccountData,
+
+    #[msg("Commission basis points must be less than or equal to the Config account's max_commission_bps.")]
+    MaxCommissionFeeBpsExceeded,
+
+    #[msg("The given MultiSigAccount is not ready to be closed.")]
+    PrematureCloseMultiSigAccount,
+
+    #[msg("The given ClaimStatus account is not ready to be closed.")]
+    PrematureCloseClaimStatus,
+
+    #[msg("Unauthorized signer.")]
+    Unauthorized,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [Config::SEED],
+        bump,
+        payer = initializer,
+        space = Config::SIZE,
+        rent_exempt = enforce
+    )]
+    pub config: Account<'info, Config>,
+
+    pub system_program: Program<'info, System>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateConfig<'info> {
+    #[account(mut, rent_exempt = enforce)]
+    pub config: Account<'info, Config>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+}
+
+impl UpdateConfig<'_> {
+    fn auth(ctx: &Context<UpdateConfig>) -> Result<()> {
+        if ctx.accounts.config.authority != ctx.accounts.authority.key() {
+            Err(Unauthorized.into())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(
+    _validator_commission_bps: u16,
+    _bump: u8
+)]
+pub struct InitializeMultiSigAccount<'info> {
+    pub config: Account<'info, Config>,
+    #[account(
+        init,
+        seeds = [
+            MultiSigAccount::SEED,
+            validator_vote_account.key().as_ref(),
+            Clock::get().unwrap().epoch.to_le_bytes().as_ref(),
+        ],
+        bump,
+        payer = signer,
+        space = MultiSigAccount::SIZE,
+        rent_exempt = enforce
+    )]
+    pub multisig_account: Account<'info, MultiSigAccount>,
+    pub validator_vote_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct UpdateMultiSigApproval<'info> {
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        close = validator_vote_account,
+        seeds = [
+            MultiSigAccount::SEED,
+            validator_vote_account.key().as_ref(),
+        ],
+        bump = multisig_account.bump,
+    )]
+    pub multisig_account: Account<'info, MultiSigAccount>,
+    #[account(mut)]
+    pub validator_vote_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+impl UpdateMultiSigApproval<'_> {
+    fn auth(ctx: &Context<UpdateMultiSigApproval>) -> Result<()> {
+        if ctx.accounts.signer.key() != ctx.accounts.config.block_builder_authority.key()
+            || ctx.accounts.signer.key() != ctx.accounts.multisig_account.validator_authority.key()
+        {
+            Err(Unauthorized.into())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Accounts)]
+pub struct UpdateMultiSigCommission<'info> {
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        close = validator_vote_account,
+        seeds = [
+            MultiSigAccount::SEED,
+            validator_vote_account.key().as_ref(),
+        ],
+        bump = multisig_account.bump,
+    )]
+    pub multisig_account: Account<'info, MultiSigAccount>,
+    #[account(mut)]
+    pub validator_vote_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+impl UpdateMultiSigCommission<'_> {
+    fn auth(ctx: &Context<UpdateMultiSigCommission>) -> Result<()> {
+        if ctx.accounts.signer.key() != ctx.accounts.config.block_builder_authority.key()
+            || ctx.accounts.signer.key() != ctx.accounts.multisig_account.validator_authority.key()
+        {
+            Err(Unauthorized.into())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Accounts)]
+pub struct CloseMultiSigAccount<'info> {
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        close = validator_vote_account,
+        seeds = [
+            MultiSigAccount::SEED,
+            validator_vote_account.key().as_ref(),
+        ],
+        bump = multisig_account.bump,
+    )]
+    pub multisig_account: Account<'info, MultiSigAccount>,
+    #[account(mut)]
+    pub validator_vote_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+impl CloseMultiSigAccount<'_> {
+    fn auth(ctx: &Context<CloseMultiSigAccount>) -> Result<()> {
+        if ctx.accounts.signer.key() != ctx.accounts.config.block_builder_authority.key() {
+            Err(Unauthorized.into())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+// Events
+
+#[event]
+pub struct ConfigUpdatedEvent {
+    /// Who updated it.
+    authority: Pubkey,
+}
+
+#[event]
+pub struct MultiSigAccountInitializedEvent {
+    pub multisig_account: Pubkey,
+}
+
+#[event]
+pub struct UpdateMultiSigApprovalEvent {
+    pub multisig_account: Pubkey,
+}
+
+#[event]
+pub struct UpdateMultiSigCommissionEvent {
+    pub multisig_account: Pubkey,
+    pub operator_commission: u16,
+    pub block_builder_commission: u16,
+}
+
+#[event]
+pub struct MultiSigAccountClosedEvent {
+    pub multisig_account: Pubkey,
+    pub amount_claimed: u64,
+}
---
+++ programs/multisig/src/sdk/instruction.rs
@@ -0,0 +1,237 @@
+use anchor_lang::{
+    prelude::Pubkey, solana_program::instruction::Instruction, InstructionData, ToAccountMetas,
+};
+
+use crate::Config;
+
+pub struct InitializeArgs {
+    pub authority: Pubkey,
+    pub block_builder_authority: Pubkey,
+    pub block_builder_commission_account: Pubkey,
+    pub block_builder_commission_bps: u16,
+    pub max_validator_commission_bps: u16,
+    pub bump: u8,
+}
+pub struct InitializeAccounts {
+    pub config: Pubkey,
+    pub system_program: Pubkey,
+    pub initializer: Pubkey,
+}
+pub fn initialize_ix(
+    program_id: Pubkey,
+    args: InitializeArgs,
+    accounts: InitializeAccounts,
+) -> Instruction {
+    let InitializeArgs {
+        authority,
+        block_builder_authority,
+        block_builder_commission_account,
+        block_builder_commission_bps,
+        max_validator_commission_bps,
+        bump,
+    } = args;
+
+    let InitializeAccounts {
+        config,
+        system_program,
+        initializer,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::Initialize {
+            authority,
+            block_builder_authority,
+            block_builder_commission_account,
+            block_builder_commission_bps,
+            max_validator_commission_bps,
+            bump,
+        }
+        .data(),
+        accounts: crate::accounts::Initialize {
+            config,
+            system_program,
+            initializer,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct UpdateConfigArgs {
+    new_config: Config,
+}
+pub struct UpdateConfigAccounts {
+    pub config: Pubkey,
+    pub authority: Pubkey,
+}
+pub fn update_config_ix(
+    program_id: Pubkey,
+    args: UpdateConfigArgs,
+    accounts: UpdateConfigAccounts,
+) -> Instruction {
+    let UpdateConfigArgs { new_config } = args;
+
+    let UpdateConfigAccounts { config, authority } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::UpdateConfig { new_config }.data(),
+        accounts: crate::accounts::UpdateConfig { config, authority }.to_account_metas(None),
+    }
+}
+
+pub struct InitializeMultiSigAccountArgs {
+    pub validator_commission_bps: u16,
+    pub bump: u8,
+}
+pub struct InitializeMultiSigAccountAccounts {
+    pub config: Pubkey,
+    pub signer: Pubkey,
+    pub system_program: Pubkey,
+    pub multisig_account: Pubkey,
+    pub validator_vote_account: Pubkey,
+}
+pub fn initialize_multi_sig_account_ix(
+    program_id: Pubkey,
+    args: InitializeMultiSigAccountArgs,
+    accounts: InitializeMultiSigAccountAccounts,
+) -> Instruction {
+    let InitializeMultiSigAccountArgs {
+        validator_commission_bps,
+        bump,
+    } = args;
+
+    let InitializeMultiSigAccountAccounts {
+        config,
+        multisig_account,
+        system_program,
+        validator_vote_account,
+        signer,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::InitializeMultiSigAccount {
+            validator_commission_bps,
+            bump,
+        }
+        .data(),
+        accounts: crate::accounts::InitializeMultiSigAccount {
+            config,
+            signer,
+            system_program,
+            multisig_account,
+            validator_vote_account,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct UpdateMultiSigApprovalArgs {
+    pub grant_approval: bool,
+}
+pub struct UpdateMultiSigApprovalAccounts {
+    pub config: Pubkey,
+    pub multisig_account: Pubkey,
+    pub validator_vote_account: Pubkey,
+    pub signer: Pubkey,
+}
+pub fn update_multi_sig_approval_ix(
+    program_id: Pubkey,
+    args: UpdateMultiSigApprovalArgs,
+    accounts: UpdateMultiSigApprovalAccounts,
+) -> Instruction {
+    let UpdateMultiSigApprovalArgs { grant_approval } = args;
+
+    let UpdateMultiSigApprovalAccounts {
+        config,
+        multisig_account,
+        validator_vote_account,
+        signer,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::UpdateMultiSigApproval { grant_approval }.data(),
+        accounts: crate::accounts::UpdateMultiSigApproval {
+            config,
+            validator_vote_account,
+            multisig_account,
+            signer,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct UpdateMultiSigCommissionArgs {
+    pub validator_commission_bps: Option<u16>,
+}
+pub struct UpdateMultiSigCommissionAccounts {
+    pub config: Pubkey,
+    pub multisig_account: Pubkey,
+    pub validator_vote_account: Pubkey,
+    pub signer: Pubkey,
+}
+pub fn update_multi_sig_commission_ix(
+    program_id: Pubkey,
+    args: UpdateMultiSigCommissionArgs,
+    accounts: UpdateMultiSigCommissionAccounts,
+) -> Instruction {
+    let UpdateMultiSigCommissionArgs {
+        validator_commission_bps,
+    } = args;
+
+    let UpdateMultiSigCommissionAccounts {
+        config,
+        multisig_account,
+        validator_vote_account,
+        signer,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::UpdateMultiSigCommission {
+            validator_commission_bps,
+        }
+        .data(),
+        accounts: crate::accounts::UpdateMultiSigCommission {
+            config,
+            validator_vote_account,
+            multisig_account,
+            signer,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct CloseMultiSigAccountArgs;
+pub struct CloseMultiSigAccounts {
+    pub config: Pubkey,
+    pub multisig_account: Pubkey,
+    pub validator_vote_account: Pubkey,
+    pub signer: Pubkey,
+}
+pub fn close_multi_sig_account_ix(
+    program_id: Pubkey,
+    _args: CloseMultiSigAccountArgs,
+    accounts: CloseMultiSigAccounts,
+) -> Instruction {
+    let CloseMultiSigAccounts {
+        config,
+        multisig_account,
+        validator_vote_account,
+        signer,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::CloseMultiSigAccount {}.data(),
+        accounts: crate::accounts::CloseMultiSigAccount {
+            config,
+            validator_vote_account,
+            multisig_account,
+            signer,
+        }
+        .to_account_metas(None),
+    }
+}
